58cca14b9f8454929fccd42f35609d55a178b5fc
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-14 15:50:59 +0100

    Update all.dart

--- lib/model/categories/all.dart
+++ lib/model/categories/all.dart
@@ -1,20 +1,54 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
 class AllProduct {
   String name; 
   String price; 
-  String image; 
+  Image product; 
   String category; 
   String reviews; 
   AllProduct({
     required this.name,
     required this.price,
-    required this.image,
+    required this.product,
+    required this.category,
     required this.reviews,
-    required this.category, 
   });
 
 }
 
 List<AllProduct> allProducts = [
-  AllProduct(name: 'Chicken Beef', price: price, image: image, reviews: reviews)
+  AllProduct(
+    name: 'Chicken Beef', 
+    price: '120',
+     product: Image.asset('assets/products/all/1.jpg'), 
+     reviews: '50 Reviews', 
+     category: 'Beef'), 
+
+      AllProduct(
+    name: 'Chicken Beef', 
+    price: '120',
+     product: Image.asset('assets/products/all/2.jpg'), 
+     reviews: '50 Reviews', 
+     category: 'Beef'),  AllProduct(
+    name: 'Chicken Beef', 
+    price: '120',
+     product: Image.asset('assets/products/all/3.jpg'), 
+     reviews: '50 Reviews', 
+     category: 'Beef'),  AllProduct(
+    name: 'Chicken Beef', 
+    price: '120',
+     product: Image.asset('assets/products/all/4.jpg'), 
+     reviews: '50 Reviews', 
+     category: 'Beef'),  AllProduct(
+    name: 'Chicken Beef', 
+    price: '120',
+     product: Image.asset('assets/products/all/5.jpg'), 
+     reviews: '50 Reviews', 
+     category: 'Beef'),  AllProduct(
+    name: 'Chicken Beef', 
+    price: '120',
+     product: Image.asset('assets/products/all/6.jpg'), 
+     reviews: '50 Reviews', 
+     category: 'Beef'), 
 ]; 
