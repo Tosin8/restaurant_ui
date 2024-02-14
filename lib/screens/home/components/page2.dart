7657c50d9a0c57bc7a5ebb6485f80adc9c61917e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-14 17:17:54 +0100

    Updating

--- lib/screens/home/components/page2.dart
+++ lib/screens/home/components/page2.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:restuarant_ui/constants.dart';
 
@@ -17,7 +18,7 @@ class _Page2State extends State<Page2> {
   Widget build(BuildContext context) {
     //Size size = MediaQuery.of(context).size;
     return Container(
-      height: 400, 
+      height: 800, 
       //width: size.width,
       width: double.infinity,
       child:  Padding(
@@ -29,40 +30,54 @@ class _Page2State extends State<Page2> {
             style: Header1), 
              const SizedBox(height: 50), 
          
-Row(
- 
-  children: [
-    hotDessertCard(
-      food: Image.asset('assets/images/hot_desset/1.png'), 
-      title: 'Chicken Fry',
-       price: '\$50',
-       ),
-       SizedBox(width: 15), 
-       hotDessertCard(
-      food: Image.asset('assets/images/hot_desset/2.png'), 
-      title: 'Chicken Fry',
-       price: '\$50',
-       ),SizedBox(width: 15), hotDessertCard(
-      food: Image.asset('assets/images/hot_desset/3.png'), 
-      title: 'Chicken Fry',
-       price: '\$50',
-       ),SizedBox(width: 15), hotDessertCard(
-      food: Image.asset('assets/images/hot_desset/4.png'), 
-      title: 'Chicken Fry',
-       price: '\$50',
-       ),SizedBox(width: 15), 
-       hotDessertCard(
-      food: Image.asset('assets/images/hot_desset/5.png'), 
-      title: 'Chicken Fry',
-       price: '\$50',
-       ),
-  ],
-), 
-SizedBox(height: 30), 
-Text('Choose from popular categories'.toUpperCase(), style: Header1,)
-             ],)
+const hotDessertRow(), 
+const SizedBox(height: 30), 
+Text('Choose from popular categories'.toUpperCase(), 
+style: Header1,), 
+const SizedBox(height: 30),
+  Container(
+    height: 300, width: 200,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(30), 
+    ), 
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10), 
+             topRight: Radius.circular(10), 
+          ),
+          child: Container(
+            height: 120, width: 180,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/products/all/4.jpg',),
+                 fit: BoxFit.cover)
+            ),
+            child: const Align(
+              alignment: Alignment.topRight,
+              child: Icon(FontAwesomeIcons.heart)),
+          ),
+        ),
+        const Text('Chicken Flavor', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800, color: Colors.black),), 
+        const Divider(), 
+        const Row(
+          children: [
+            Text('60 Reviews'),
+            SizedBox(height: 20,
+             child: VerticalDivider(width: 10, thickness: 1, color: Colors.black,),), 
+             
+          ],
+        )
+      ]
+    
+    
+    
+               ),
+  ), ] ))); 
           
-     ) );
+     
       
     
   }
