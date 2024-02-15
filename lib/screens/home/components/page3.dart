ee82f97cedb77af40d0ed540a51d571eef32a51f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-15 17:10:20 +0100

    adding food receipe of the week

--- lib/screens/home/components/page3.dart
+++ lib/screens/home/components/page3.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:restuarant_ui/constants.dart';
+import 'package:simple_shadow/simple_shadow.dart';
 class Page3 extends StatelessWidget {
   const Page3({super.key});
 
@@ -7,9 +8,24 @@ class Page3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 400,
-      child: Row(
-        children: [],
+      height: 800,
+      child: Padding(
+        padding: EdgeInsets.only(left: 80), 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('RECEIPE OF THE WEEK', style: Header1,), 
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Container(
+                  child: SimpleShadow(
+                    child: Image.asset('assets/products/receipe/2.png')),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
