5bf1d909bf3a60bab84a7c842e818b6e7c5f0578
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-16 04:46:49 +0100

    adding icons for mobile app

--- lib/screens/home/components/page6.dart
+++ lib/screens/home/components/page6.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restuarant_ui/constants.dart';
+import 'package:simple_shadow/simple_shadow.dart';
 
 class Page6 extends StatelessWidget {
   const Page6({super.key});
@@ -17,11 +18,31 @@ class Page6 extends StatelessWidget {
       child: Row(
         children: [
           Container(
-            child: Image.asset('assets/banner/phone.png')
+            child: SimpleShadow(
+              offset: Offset(3, 3),
+              child: Image.asset('assets/banner/phone.png'))
           ), 
-          const Column(children: [
-            Text('Now with your favorite\nBelly Max mobile app in your pocket')
-          ],)
+          Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: Column(
+              
+              children: [
+              Text('Now with your favorite\nBelly Max mobile app in your pocket', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w400),
+              ), 
+              SizedBox(height: 20),
+              Container(
+                height: 80, width: 300,
+decoration: BoxDecoration(
+  color: Colors.white, 
+borderRadius: BorderRadius.circular(30)),
+child: Row(
+  children: [
+    
+  ],
+),
+              )
+            ],),
+          )
         ],
       ),
     );
