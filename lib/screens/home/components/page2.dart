f6f1d30f9b313bbb73be3fef280d66ce4923b9dd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-16 14:58:22 +0100

    Update page2.dart

--- lib/screens/home/components/page2.dart
+++ lib/screens/home/components/page2.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 
 import 'package:restuarant_ui/constants.dart';
 
 import '../home_widget.dart';
-
 
 class Page2 extends StatefulWidget {
   const Page2({super.key});
@@ -36,7 +36,7 @@ const SizedBox(height: 30),
 Text('Choose from popular categories'.toUpperCase(), 
 style: Header1,), 
 const SizedBox(height: 30),
-  allProductCard(), ] ))); 
+  const allProductCard(), ] ))); 
           
      
       
@@ -46,9 +46,19 @@ const SizedBox(height: 30),
 
 class allProductCard extends StatelessWidget {
   const allProductCard({
-    super.key,
-  });
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.price,
+    required this.review,
+    required this.calories,
+  }) : super(key: key);
 
+final Image image; 
+final String title; 
+final double price;
+final String review; 
+final String calories;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -83,7 +93,7 @@ class allProductCard extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: 10,), 
+          const SizedBox(height: 10,), 
           const Text('Chicken Flavor', 
           style: TextStyle(fontSize: 18, 
           fontWeight: FontWeight.w800, 
@@ -104,18 +114,11 @@ class allProductCard extends StatelessWidget {
           const SizedBox(height: 5,), 
            Row(
             children: [
-              Text('\$50.00', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),), 
-              SizedBox(width: 20,),
-              Icon(Icons.favorite_outline), 
-              SizedBox(width: 10,),
-              Container(
-                height: 30, width: 30,
-    decoration: BoxDecoration(
-    shape: BoxShape.circle, 
-    color: kPrimaryColor
-    ), 
-    child: Icon(Icons.add, color: Colors.white,),
-              )
+              const Text('\$50.00', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),), 
+              const SizedBox(width: 20,),
+              const Icon(Icons.favorite_outline), 
+              const SizedBox(width: 10,),
+              addButton()
     
             ],
           )
@@ -125,6 +128,20 @@ class allProductCard extends StatelessWidget {
       
                  ),
     );
+  }
+
+  GestureDetector addButton() {
+    return GestureDetector( 
+              onTap: (){},
+              child: Container(
+                height: 30, width: 30,
+                  decoration: const BoxDecoration(
+                  shape: BoxShape.circle, 
+                  color: kPrimaryColor
+                  ), 
+                  child: const Icon(Icons.add, size: 18, color: Colors.white,),
+              ),
+            );
   }
 }
 
