96b6f7a82211cf5d0e3b9486268640530e15b91b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-16 02:46:55 +0100

    Update whyus.dart

--- lib/model/whyus.dart
+++ lib/model/whyus.dart
@@ -12,6 +12,19 @@ class WhyUs {
 
 
 List <WhyUs> whyus = [
+  WhyUs(
+    title: 'Best Offer', 
+    image: 'assets/icons/why_us/best_offer.png', 
+    subtitle: 'Enjoy discounts when you make an order and you refer your friend to us. Bla Bla Bla.'), 
+    WhyUs(
+    title: 'Fash Delivery', 
+    image: 'assets/icons/why_us/fast_delivery.png', 
+    subtitle: 'Enjoy discounts when you make an order and you refer your friend to us. Bla Bla Bla'), 
+    WhyUs(
+    title: 'Fresh Food', 
+    image: 'assets/icons/why_us/cash_on_delivery.png', 
+    subtitle: 'Enjoy discounts when you make an order and you refer your friend to us. Bla Bla Bla'), 
+ 
 
 ]; 
 }
